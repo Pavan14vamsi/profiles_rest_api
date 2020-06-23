3964b2d8f8f350305a30ad38aedf4c3e056a581d
Pavan Vamsi Tadikonda <pavan14vamsi@gmail.com>
2020-06-23 15:46:40 +0530

    Added feed, permissions for feed

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -9,3 +9,12 @@ class UpdateOwnProfile(permissions.BasePermission):
             return True
 
         return obj.id == request.user.id #So we'll give a user id
+
+class UpdateOwnStatus(permissions.BasePermission):
+    """Ensures only their own statuses are changeable"""
+    def has_object_permission(self, request, view, obj):
+        """Checks if it's their own status"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.user_profile.id == request.user.id
--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -6,7 +6,7 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register("helloViewset", views.HelloViewSet, base_name = "helloViewset")
 router.register("profile", views.UserProfileViewSet)
-
+router.register("feed", views.UserProfileFeedViewSet)
 urlpatterns = [
     path("an_apiview/", views.HelloApiView.as_view()), #The HelloApiView is a rest_framework class that's why you need the .as_view
     path('login/', views.UserLoginApiView.as_view()),
